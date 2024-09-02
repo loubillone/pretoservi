eeb13f3eb23e2545eef27b621728732453e7b111
Lourdes Billone <lou.billone@gmail.com>
2024-09-02 11:13:59 -0300

    navbar elementos centrados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navegador from "./components/Navegador";
 
 const App = () => {
   return (
     <div>
-      <h1>Petroservi</h1>
+      <Navegador />
     </div>
   );
 };
---
+++ src/components/Navegador.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../css/navegador.css";
+import logoPetro from "../assets/img/logoPetro.png";
+
+const Navegador = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logoPetro} alt="logo petroservi" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto nav-container-items">
+              <Nav.Link href="#home" className="nav-items">
+                Inicio
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Sobre Nosotros
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Empresas Mineras
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Galeria
+              </Nav.Link>
+              <Nav.Link href="#link" className="nav-items">
+                Contacto
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navegador;
