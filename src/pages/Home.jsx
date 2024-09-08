624ffd76eaaa0a593e4d146b4ac2f102b1e3dae7
Lourdes Billone <lou.billone@gmail.com>
2024-09-08 12:08:43 -0300

    caja de texto sobre imagen home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,17 @@ const Home = () => {
     <div>
       <div className="fila-imagen-home">
         <img src={homeFlyer} alt="camioneta petroservi" className="img-home" />
+        <div className="container-textoImg-home">
+          <h1 className="titulo-img-home">PetroServi</h1>
+          <p className="subtitulo-img-home">Empresa Homologada por YPF SA.</p>
+          <p className="texto-img-home">
+            Tanques de combustible y agua, equipos de despacho completos,
+            bombas, caudalímetros, sistemas de filtrado, mangueras
+            especializadas, cisternas móviles y más. ¡Encuentra lo que necesitas
+            para mantener tu operación funcionando al máximo!
+          </p>
+          <button className=" boton-img-home">Contactanos</button>
+        </div>
       </div>
     </div>
   );
