60470b1a04e6bee001569e08ffe4ab3a5d8e2f63
Lourdes Billone <lou.billone@gmail.com>
2024-09-11 20:26:50 -0300

    posicion de imagenes y textos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import homeFlyer from "../assets/img/home/homeFlyer.png";
 import mano from "../assets/img/home/mano.png";
 import shape from "../assets/img/home/shape.png";
 import tanque from "../assets/img/home/tanque.png";
+import homeNosotros from "../assets/img/home/homeNosotros.png";
+import homeNosotrosDos from "../assets/img/home/homeNosotrosDos.png";
 
 const Home = () => {
   return (
@@ -80,6 +82,38 @@ const Home = () => {
                   Suministros esenciales para garantizar la operación.
                 </p>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container container-home-nosotros">
+        <div className="row fila-home-nosotros">
+          <div className="col">
+            <div>
+              <span>Sobre nosotros</span>
+              <h2>Soluciones Innovadoras para el Éxito Industrial</h2>
+              <h3>Comprometidos con la Excelencia y la Innovación</h3>
+              <p>
+                En PetroServi, aplicamos nuestro profundo conocimiento en la
+                instalación y mantenimiento de sistemas industriales para
+                asegurar el éxito en cada proyecto. Ofrecemos soluciones
+                integrales para estaciones de servicio, incluyendo tanques de
+                combustible, equipos de despacho, cisternas móviles y más.
+                Nuestro equipo está comprometido con la calidad y la innovación,
+                garantizando un servicio confiable y eficiente para la industria
+                petrolera y más allá.
+              </p>
+              <button className="boton-img-home">Sobre Nosotros</button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="imagen-home-nosotrosUno">
+              <img src={homeNosotrosDos} alt="vista arriba" />
+            </div>
+
+            <div className="imagen-home-nosotrosDos">
+              <img src={homeNosotros} alt="surtidor" />
             </div>
           </div>
         </div>
